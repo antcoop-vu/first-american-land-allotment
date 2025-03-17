844f510c0c9858aa840bc24f72cb1218d0e78241
lenawuu <lenawu24@gmail.com>
2025-03-17 12:35:39 -0500

    got map to render kind of

---
+++ components/data-dashboard/map-dashboard.tsx
@@ -0,0 +1,29 @@
+'use client'
+
+import { useRef } from "react";
+import { MapContainer, TileLayer, GeoJSON, ZoomControl } from "react-leaflet"
+
+export default function MapDashboard() {
+  const mapRef = useRef(null)
+  
+  return (
+    <div className="bg-cream/50 rounded-lg shadow-md overflow-hidden">
+      <div className="col-span-3 h-[600px] relative">
+        {typeof window !== "undefined" && (
+        <MapContainer
+          center={[39.8283, -98.5795]}
+          zoom={4}
+          style={{ height: "100%", width: "100%" }}
+          zoomControl={false}
+          ref={mapRef}
+        >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        </MapContainer>
+      )}
+      </div>
+    </div>
+  );
+}
